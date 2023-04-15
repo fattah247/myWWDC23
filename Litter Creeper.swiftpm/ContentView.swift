3483b3edcbdc53c1d93ca39c1ab6660287af7d33
fattah247 <muhammad.abdul92@ui.ac.id>
2023-04-15 16:05:00 +0700

    Matching Prototype

--- Litter Creeper.swiftpm/ContentView.swift
+++ Litter Creeper.swiftpm/ContentView.swift
@@ -1,12 +1,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isDragging = false
+    @State var position = CGSize.zero
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        RoundedRectangle(cornerRadius: 20, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
+            .frame(width: 250, height: 200)
+            .offset(x: position.width, y: position.height)
+            .foregroundColor(.blue)
+            .gesture(
+                DragGesture()
+                    .onChanged({ value in
+                        position = value.translation
+                        isDragging = true
+                })
+                    .onEnded({ value in
+                        position = .zero
+                        isDragging = false
+                    })
+            )
     }
 }
